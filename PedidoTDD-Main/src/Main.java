67496ccaf14551fbdc7b85290257760e1ba6fda8
ZeItal0 <intaloutins2058@gmail.com>
2024-11-27 23:11:27 -0300

    Add files via upload
    Criei duas funcionalidades para oque foi proposto em sala sobre (TDD) , remover item e aplicar imposto sobre o item.

---
+++ PedidoTDD-Main/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        Pedido pedido = new Pedido();
+        pedido.adicionarItem("Produto A", 2, 50.0);
+        pedido.adicionarItem("Produto B", 1, 120.0);
+
+        System.out.println("Itens do Pedido:");
+        pedido.getItens().forEach(item -> {
+            System.out.println("- " + item.getSubtotal());
+        });
+
+        System.out.println("Total sem desconto: " + pedido.getTotal());
+
+        pedido.aplicarDesconto();
+        System.out.println("Total com desconto: " + pedido.getTotal());
+        
+        
+        System.out.println("imposto de 18%:" + pedido.imposto());
+        System.out.println("removendo produto B");
+        pedido.removerUmItem("Produto B");
+        
+    }
+}
